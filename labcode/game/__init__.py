990f1fc6afee68ded6a23cb96068b85760b884b2
Victor Tan <victor.tan.33@gmail.com>
2023-07-22 08:40:42 +0800

    update with exercise solutions

--- labcode/game/__init__.py
+++
@@ -1,7 +0,0 @@
-
-
-print ("Initialization from init.py")
-
-
-name_of_game = "Awesome game"
-top_score = [100, 20, 30]
--- labcode/game/sound/play.py
+++
@@ -1,3 +0,0 @@
-
-def make_cool_sounds():
-    print ("Cool game sounds from play module in sound subpackage")
